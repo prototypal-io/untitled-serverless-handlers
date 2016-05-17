12f573c21896275fd84bdb03774808405ca58622
Jeremy Green <jeremy@octolabs.com>
2016-05-16 22:35:45 -0500

    Sketch out the express style handler
    TODO:
    * Update the `handler` function to call `endpontLogic` presenting objects for `req` and `res`
    * Update `s-function.json` to pass as much of the request as possible to the serverless `handler`

--- expressModel/handler.js
+++ expressModel/handler.js
@@ -1,5 +1,13 @@
 'use strict';
 
+var endpointLogic = function(req, res) {
+  var echo = req.params.echo || "Say something.";
+  res.json({
+    echo: echo,
+    sourceIp: req.ip
+  });
+}
+
 module.exports.handler = function(event, context, cb) {
   return cb(null, {
     message: 'Go Serverless! Your Lambda function executed successfully!'
